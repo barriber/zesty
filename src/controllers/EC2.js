62b9ce9edb041734752cd3b73f5930b3570088ba
bariber <boris.berezovsky@safebreach.com>
2023-06-18 00:57:33 +0300

    small fixes

--- src/controllers/EC2.js
+++ src/controllers/EC2.js
@@ -1,6 +1,20 @@
-const instancesFile = require('../$region_name.json')
+const path = require("path");
+
 class EC2 {
+
+    fetchInstances() {
+        const instancePath = path.join(__dirname, '../../$region_name.json');
+        const instancesFile = require(instancePath)
+
+        return instancesFile
+    }
     async getInstancesByRegion(req, res) {
+        try {
+            this.fetchInstances();
+        } catch (e) {
+            return res.status(500).json({ error: 'Failed to fetch instances' });
+        }
+
         const {query: { region }} = req;
         if(!region) {
             const errorMessage = 'Missing region input.';
--- src/etl.js
+++ src/etl.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const EC2 = require('./utils/EC2');
 const FileHandler = require('./utils/FileHanlder');
 
@@ -9,16 +10,16 @@ async function getRegions(filePath) {
 
 }
 async function writeEc2Instances() {
-    const filePath = '../regions.txt';
-    const regions = await getRegions(filePath)
-
+    const regionsPath = path.join(__dirname, '../regions.txt');
+    const regions = await getRegions(regionsPath)
     const ec2 = new EC2();
     const instancesArray = await Promise.all(regions.map(region => ec2.getEc2Instances(region)));
     const instances = [].concat(...instancesArray);
     const instancesWithParsedDate = instances.map(instance => ({...instance, launchTimeParsed:  new Date(instance.LaunchTime).getTime()}))
     instancesWithParsedDate.sort((instanceA, instanceB) => instanceA.launchTimeParsed - instanceB.launchTimeParsed);
 
-    await FileHandler.writeToFile('./$region_name.json', instancesWithParsedDate)
+    const resultPath = path.join(__dirname, '../$region_name.json');
+    await FileHandler.writeToFile(resultPath, instancesWithParsedDate)
 }
 
 module.exports = { writeEc2Instances }
